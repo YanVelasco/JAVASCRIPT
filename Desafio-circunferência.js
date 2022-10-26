0f6515b2d0b92f59b0eb069a97d546d1d5f99488
Yan_Velasco <116385013+YanVelasco@users.noreply.github.com>
2022-10-26 14:26:22 -0300

    JAVASCRIPT
    commit JAVASCRIPT

---
+++ Desafio-circunferência.js
@@ -0,0 +1,9 @@
+// pi * (Raio * Raio) = ÁREA DA CIRCUNFEÊNCIA
+
+const pi = 3.14
+let raio = 10
+
+
+let area = pi  * (raio * raio)
+
+console.log("A áre da circunferêcia é:", area,"metros quadrados")
---
+++ Organizção-do-código.js
@@ -0,0 +1,8 @@
+console.log("Sentença de código");
+ 
+{//Um bloco agrupa sentenças de códigos
+     {//Um bloco pode estar dentro de outro bloco
+        console.log("Olá");
+        console.log("mundo");
+     }
+}
